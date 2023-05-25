b5da273a89631f9074e7c1d1ed05d3ff7a176a4a
Yonathan Castillo <leothan522@gmail.com>
2023-05-24 20:47:54 -0400

    Enlace Telegram

--- resources/views/dashboard/footer.blade.php
+++ resources/views/dashboard/footer.blade.php
@@ -2,8 +2,5 @@
     <b>Version</b> 1.0
 </div>
 <strong>&copy; 2023 {{ config('app.name') }}
-    | <span class="text-lightblue">Ing. Yonathan Castillo.</span>
+    | <a href="https://t.me/Leothan" target="_blank">Ing. Yonathan Castillo</a>
 </strong>
-{{--<em class="text-sm">
-    Basado en <a href="http://adminlte.io" target="_blank">AdminLTE.io</a>.
-</em>--}}
